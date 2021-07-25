9c14cf6dd0d9927a0e9329d2cbc15b19037465c2
Anup Kumar Dutta <anupdutta45@gmail.com>
2021-07-25 18:19:11 +0530

    Adding first java code in my maven project

---
+++ src/main/java/anup/programming/training/AppliCode.java
@@ -0,0 +1,15 @@
+package anup.programming.trainig;
+
+import java.util.*;
+
+class AppliCode
+{
+	public static void main(String args[])
+	{
+		ArrayList<String> ar=new ArrayList<String>();
+		ar.add("Anup");
+		ar.add("Kumar");
+		ar.add("Dutta");
+		System.out.println(ar);	
+	}
+}
